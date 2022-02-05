d85cd58272ea4879f0f003e8552c9ed142c7cf68
JHessler <jhessler11@gmail.com>
2022-02-05 15:40:30 -0500

    added hourtracker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
+const times = ['8am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
+var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a")
+
 
 // displays current time to top of page
 var displayTime = function() {
-    var currentTime = moment().format("MMM Do YY")
     $("#currentDay").html(currentTime)
 }
 displayTime()
 
-
-
+var hourTracker = function() {
+    $('time.block').each(function(){
+        var eachHour = parseInt($('time.block').attr('id').split('hour')[1])
+        
+    })
+}
+    
 //loop to check for hours 
